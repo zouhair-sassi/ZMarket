3db0bdec303ccf3fe6eb3084a496ab79eb383b1b
zouhair sassi <zouhair.sassi@nostatikmedia.com>
2020-09-22 20:52:42 +0100

    addCategoryController

---
+++ ZMarket/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  ZMarket
+//
+//  Created by Zouhair Sassi on 9/22/20.
+//  Copyright © 2020 Zouhair Sassi. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
